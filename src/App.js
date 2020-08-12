a1fdd1ebcdd1251193c2272924387662674a5c76
Jon Selzer <jselzer626@gmail.com>
2020-08-12 16:45:42 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import { Modal, Image, Container } from 'semantic-ui-react'
 import logo from './logo.svg';
+import tests from './tests.json';
 import './App.css';
 
+
 function App() {
+
+  const subjects = ['English', 'Math', 'Science']
+  const years = ['2017', '2018', '2019', '2020']
+  
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="ui text container">
+
+
+
+
+      </div>
+
     </div>
   );
 }
---
+++ src/testdata.js
@@ -0,0 +1,50 @@
+/*questions - array of images
+subject - string, foreign key
+year - string, foreign key
+title - string (concat year and )
+userClicked
+totalClicks 
+*/
+
+let subjects = ['English', 'Math', 'Science']
+
+let years = ['2017', '2018', '2019', '2020']
+
+tests = {allTests: []}
+
+let createTests = () => {
+    subjects.forEach(subject => {
+        years.forEach(year => {
+            for (let i = 0; i < 10; i ++) {
+                var newTest = {}
+                newTest.subject = subject
+                newTest.year = year
+                newTest.title = `${year} ${subject} Exam`
+                questions = []
+                for (let j = 0; j < 30; j++) {
+                    question = `Question #${j}`
+                    questions.push(question)
+                }
+                newTest.questions = questions
+                newTest.userClicked = false
+                newTest.userClicks = Math.floor(Math.random() * Math.floor(20))
+                tests.allTests.push(newTest)
+            }
+        })
+    })
+}
+
+
+createTests()
+
+tests.allTests.sort((a,b) => (a.userClicks < b.userClicks) ? 1 : -1 )
+
+const fs = require("fs")
+
+fs.writeFile('tests.json', JSON.stringify(tests), err => {
+
+    if (err) throw err
+
+    console.log("Done")
+})
+
