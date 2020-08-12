1f0bb0e9e6189d29a056ff4a5f3511ddaa66f6df
Jon Selzer <jselzer626@gmail.com>
2020-08-12 17:38:29 -0400

    finished test data

--- src/App.js
+++ src/App.js
@@ -3,14 +3,44 @@ import { Modal, Image, Container } from 'semantic-ui-react'
 import logo from './logo.svg';
 import tests from './tests.json';
 import './App.css';
+import 'English' from './images/English.png';
+import 'Math' from './images/Math.png';
+import 'Science' from './images/Science.png';
 
 
 function App() {
 
-  const subjects = ['English', 'Math', 'Science']
+  const testSubjects = ['English', 'Math', 'Science']
   const years = ['2017', '2018', '2019', '2020']
+  const categories = ['Trending', 'By Subject', 'By Year', 'Random']
   
+  //can be adapted for mobile or desktop view
+  // this is how many will display for each category
+  const [displayChunk, setDisplayChunk] = useState(2)
+  //tests have been sorted by userclicks
+  const [allTests, setAllTests] = useState(tests.allTests)
+  const [currentTest, setCurrentTest] = useState([])
 
+  const testCard = test => {
+    return (
+      <div className='ui card'>
+        <div className="image">
+          <img src={test.subject}/>
+        </div>
+        <div className="content">
+          <div className="header">
+            {test.title}
+          </div>
+          <div className="description">
+            This test has been viewed {test.userClicks} times
+          </div>
+        </div>
+        <div className="extraContent">
+          {test.questions.length} questions
+        </div>
+      </div>
+    )
+  }
 
 
   return (
