5e6daa0d11d522627b86379b81b605a08e3540b5
Jon Selzer <jselzer626@gmail.com>
2020-08-14 14:22:58 -0400

    Updates

---
+++ precache-manifest.5092434c2d65b20b9747aa4c4710bfd4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c13f075fdaf11ca0018b88cf6aac39",
+    "url": "/event_feed/index.html"
+  },
+  {
+    "revision": "51dc9327a1b65a2732e1",
+    "url": "/event_feed/static/css/main.c1b27a9c.chunk.css"
+  },
+  {
+    "revision": "b5edd124b325ed8bf14d",
+    "url": "/event_feed/static/js/2.3caf33f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/event_feed/static/js/2.3caf33f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51dc9327a1b65a2732e1",
+    "url": "/event_feed/static/js/main.003afa0d.chunk.js"
+  },
+  {
+    "revision": "0c7e796316b1ad254042",
+    "url": "/event_feed/static/js/runtime-main.4d2c9429.js"
+  },
+  {
+    "revision": "6409b912ebeca86bfdbdc1407e36b01d",
+    "url": "/event_feed/static/media/English.6409b912.png"
+  },
+  {
+    "revision": "efafea32fe520695c8112e9a4286ee28",
+    "url": "/event_feed/static/media/Math.efafea32.png"
+  },
+  {
+    "revision": "31c36b16b0a03c94e7ebebd3d51c024c",
+    "url": "/event_feed/static/media/Science.31c36b16.png"
+  }
+]);
